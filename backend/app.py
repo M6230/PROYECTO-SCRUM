d522bbbd28605af7e50cf8f76cbcbaea20d53124
ThisIsSantiX <40646266+ThisIsSantiX@users.noreply.github.com>
2025-06-16 14:38:26 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,5 +17,5 @@ app.mysql = mysql
 
 registrar_rutas(app)
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0") 
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0") 
